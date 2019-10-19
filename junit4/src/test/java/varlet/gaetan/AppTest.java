26cb5f246b9a81d5a3fb2f8b98c720d8294f7a3c
Gaëtan Varlet <gaetanvarlet@gmail.com>
2019-10-19 15:40:30 +0200

    ajout test en JUnit 4

---
+++ junit4/src/test/java/varlet/gaetan/AppTest.java
@@ -0,0 +1,19 @@
+package varlet.gaetan;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert.assertEquals;
+import org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class AppTest {
+
+    App app = new App();
+
+    @Test
+    public void testSum() {
+        assertEquals(3, app.sum(1, 2));
+        assertEquals(-2, app.sum(3, -5));
+    }
+}
